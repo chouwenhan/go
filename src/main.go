72f714fa7ab5b9b3bec2cfa06ada0bc9fc59aff8
chouwenhan <a29554483@gmail.com>
2019-02-19 23:16:32 +0800

    add-cors

--- src/main.go
+++ src/main.go
@@ -4,12 +4,24 @@ import (
     "github.com/gin-gonic/gin"
     "src/controllers"
     "src/models"
+    "fmt"
 )
+
+// middleware
+func CORSMiddleware(c *gin.Context){
+    c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+    c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+    c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+    c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+    c.Next()
+}
 
 func main() {
     models.CreateDatabase("article")
     // gin start
     router := gin.Default()
+    router.Use(CORSMiddleware)
     router.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "pong",
@@ -24,6 +36,7 @@ func main() {
     // Primary data initialization
     graphql := router.Group("/graphql")
     {
+        fmt.Println("123")
         graphql.Any("/article", controllers.ArticleHandler())
     }
 
