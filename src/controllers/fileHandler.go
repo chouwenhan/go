5f0ad8e7af1fc9f50b23cc20e5c673dccbaee688
chouwenhan <a29554483@gmail.com>
2019-07-15 23:54:08 +0800

    modify upload file api

--- src/controllers/fileHandler.go
+++ src/controllers/fileHandler.go
@@ -2,26 +2,30 @@ package controllers
 import (
     "github.com/gin-gonic/gin"
     "src/models"
-    // "fmt"
+    "fmt"
 )
 
-
-// type File struct {
-//     doc_id string
-//     url string
-// }
 
 func FileHandler(c *gin.Context) {
     doc_id := c.Param("doc_id")
     db := models.ConnDB("article")
     result, _ := models.ReadDocument(db, doc_id)
-    file, _ := c.FormFile("file")
-    fileName := file.Filename
-    fileType := file.Header["Content-Type"][0]
-    file_open, _ := file.Open()
-    save_result, err := db.SaveAttachment(doc_id, result.Rev, fileName, fileType, file_open)
-    if err != nil {
-        panic(err)
+    form, _ := c.MultipartForm()
+    fmt.Println(form)
+    files := form.File["files"]
+    fmt.Println(files)
+    for _, file := range files {
+        fileName := file.Filename
+        fileType := file.Header["Content-Type"][0]
+        file_open, _ := file.Open()
+        save_result, err := db.SaveAttachment(doc_id, result.Rev, fileName, fileType, file_open)
+        if err != nil {
+            panic(err)
+        }
+        result, _ = models.ReadDocument(db, doc_id)
+        fmt.Println(save_result)
     }
-    c.JSON(200, gin.H{"result": save_result})
+    c.JSON(200, gin.H{
+        "message": "upload files successful",
+    })
 }
